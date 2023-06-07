a39804e9bffdeb34676483fbac98991ee6963f9d
Kylea650 <90274941+Kylea650@users.noreply.github.com>
2023-06-07 10:25:57 +0100

    splitting to separate func

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -1,19 +1,22 @@
-fn main() {
-    let input: String = std::fs::read_to_string("../day1.txt").unwrap();
-
-    let mut groups: Vec<u32> = input
+fn parse_input(input: String) -> Vec<u32> {
+    let calories = input
         .split("\n\n")
-        .map(|x: &str| {
-            x.lines()
-                .map(|x: &str| x.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
+        .map(|x| x.lines().map(|x| x.parse::<u32>().unwrap()).sum::<u32>())
         .collect::<Vec<_>>();
 
-    groups.sort();
+    calories
+}
 
-    let day1: u32 = groups[groups.len() - 1];
-    let day2: u32 = groups[groups.len() - 3..groups.len()].iter().sum::<u32>();
+fn main() {
+    let input = std::fs::read_to_string("../day1.txt").unwrap();
+
+    let mut calories = parse_input(input);
+    calories.sort();
+
+    let day1 = calories[calories.len() - 1];
+    let day2 = calories[calories.len() - 3..calories.len()]
+        .iter()
+        .sum::<u32>();
 
     println!("Day 1: {}\nDay 2: {}", day1, day2);
 }
